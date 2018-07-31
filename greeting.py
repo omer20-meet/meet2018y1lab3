7f9ea574faa9b0c8392136aa6706807deaa52724
omer20-meet <omer20@meet.mit.edu>
2018-07-31 05:10:06 -0400

    Stopping here

---
+++ greeting.py
@@ -0,0 +1,9 @@
+name = input("what's your name? ")
+print ("Hello There " + str(name.capitalize()))
+leng_name = len(name)
+print ("Your name is " + str(leng_name) + " letters long!" )
+first_letter = name[0]
+last_letter = name[-1]
+print ("The first letter of your name is " + first_letter.upper() + " and the last letter is " + last_letter.upper())
+
+
